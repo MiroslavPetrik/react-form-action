c3c55f42cb4c82e6430bcf70eaa1196e1188d929
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-12-28 13:25:25 +0100

    example: update latest version

--- example/app/subscribe/action.ts
+++ example/app/subscribe/action.ts
@@ -5,22 +5,21 @@ import { z } from "zod";
 
 const users = new Set<string>();
 
-const baseAction = formAction
+export const subscribeAction = formAction
   .input(z.object({ email: z.string().email() }))
-  .error(({ error }) => {
+  .error(async ({ error }) => {
     if (error instanceof Error) {
       return error.message;
     } else {
       return "Unexpected error";
     }
+  })
+  .run(async ({ input }) => {
+    if (users.has(input.email)) {
+      throw new Error("Already subscribed!");
+    } else {
+      users.add(input.email);
+    }
+
+    return null;
   });
-
-export const subscribeAction = baseAction.run(async ({ input }) => {
-  if (users.has(input.email)) {
-    throw new Error("Already subscribed!");
-  } else {
-    users.add(input.email);
-  }
-
-  return null;
-});
