cd85e92965b9524dcca0c253f4b9b05ca4901cf6
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-01-17 11:16:13 +0100

    feat(FormStatus): deprecated

--- src/FormStatus.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import type { RenderProp } from "react-render-prop-type";
-import { useFormStatus } from "react-dom";
-
-export function FormStatus({
-  children,
-}: RenderProp<ReturnType<typeof useFormStatus>>) {
-  return children(useFormStatus());
-}
--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,5 @@
 export * from "./Action";
 export * from "./Form";
 export * from "./createComponents";
-export * from "./FormStatus";
 export * from "./Pending";
 export * from "./ZodFieldError";
